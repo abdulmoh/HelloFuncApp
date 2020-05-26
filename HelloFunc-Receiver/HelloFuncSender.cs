95f6642885b7e6f509436c26857a2b11e312a9ad
Abdul Hameed <abdulhammed3008@gmail.com>
2020-05-26 12:44:25 +0530

    Add project files.

---
+++ HelloFunc-Receiver/HelloFuncSender.cs
@@ -0,0 +1,59 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace HelloFunc_Receiver
+{
+    public static class HelloFuncSender
+    {
+        [FunctionName("HelloFuncSender")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string name = req.Query["name"];
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string azureBaseUrl = Environment.GetEnvironmentVariable("AzureFunctionURL") + name;
+            log.LogInformation(azureBaseUrl);
+            string message = string.Empty;
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    using (HttpResponseMessage res = await client.GetAsync(azureBaseUrl))
+                    {
+                        using (HttpContent content = res.Content)
+                        {
+                            string response = await content.ReadAsStringAsync();
+                            if (response != null)
+                            {
+                                message = response;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+
+            string responseMessage = string.IsNullOrEmpty(message)
+                ? "The HTTP triggered HelloFuncSender function executed successfully. Pass a name in the query string or in the request body for a personalized response.\n\n"
+                : $"Hello, {name}. The HTTP triggered HelloFuncSender function executed successfully and called Receiver function with below message.\n\n";
+            responseMessage += message;
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
